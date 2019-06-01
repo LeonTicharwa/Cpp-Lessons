aea0619074ec7fc957bbac9669f4b54c99fb9a29
leon <lt.ticharwa@gmail.com>
2019-06-01 16:25:54 +0800

    added networking

--- networking/udp.cpp
+++ networking/udp.cpp
@@ -14,28 +14,26 @@ void error( char *msg)
 }
 int main()
 {
- int sockfd;
- sockfd = socket(AF_INET,SOCK_DGRAM,0);
- struct sockaddr_in serv,client;
- serv.sin_family = AF_INET;
- serv.sin_port = htons(8001);
- serv.sin_addr.s_addr = INADDR_ANY;
- char buffer[4];
- socklen_t l = sizeof(client);
- bind(sockfd,(struct sockaddr *)&serv,sizeof(serv));
- //socklen_t m = client;
- while(1){
- cout<<"\ngoing to recv\n";
- int rc= recvfrom(sockfd,buffer,sizeof(buffer),0,(struct sockaddr *)&client,&l);
- if(rc<0)
- {
- cout<<"ERROR READING FROM SOCKET";
- }
- cout<<"\n the message received is : "<<buffer<<endl;
- int rp= sendto(sockfd,"hi",2,0,(struct sockaddr *)&client,l);
- if(rp<0)
- {
- cout<<"ERROR writing to SOCKET";
- }
- }
+    int sockfd;
+    sockfd = socket(AF_INET,SOCK_DGRAM,0);
+    struct sockaddr_in serv,client;
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(8001);
+    serv.sin_addr.s_addr = INADDR_ANY;
+    char buffer[7];
+    socklen_t l = sizeof(client);
+    bind(sockfd,(struct sockaddr *)&serv,sizeof(serv));
+    //socklen_t m = client;
+    while(1){
+        cout<<"\nWaiting for command\n";
+        int rc= recvfrom(sockfd,buffer,sizeof(buffer),0,(struct sockaddr *)&client,&l);
+        if(rc<0){
+            cout<<"ERROR READING FROM SOCKET";
+        }
+        cout<<"\n The message received is : "<<buffer<<endl;
+        int rp= sendto(sockfd,"Recived",7,0,(struct sockaddr *)&client,l);
+        if(rp<0){
+            cout<<"ERROR writing to SOCKET";
+        }
+    }
 }
